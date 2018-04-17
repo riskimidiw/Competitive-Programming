61ef0b034f5b438bfe30b1c5b37f67025f1b462a
riskimidiw <riskimidiw@gmail.com>
2018-04-17 22:18:48 +0700

    Add Basic Data Types

---
+++ C++/Introduction/Basic-Data-Types.cpp
@@ -0,0 +1,13 @@
+// Problem : https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a; long b; char c; float d; double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf\n", a, b, c, d, e);
+    return 0;
+}
