cf535aece871fefe1072c140acc284394c8fe6f6
riskimidiw <riskimidiw@gmail.com>
2018-08-04 05:18:21 +0700

    Refactor D.KasurRusak solution

--- TOKI/KelasPembelajaranPemrograman/PemrogramanDasar/12-Rekursi/D.KasurRusak/Main.java
+++ TOKI/KelasPembelajaranPemrograman/PemrogramanDasar/12-Rekursi/D.KasurRusak/Main.java
@@ -6,12 +6,11 @@ public class Main {
     
     static boolean isPalindrome(String s) {
         if(s.length() <= 1) return true;
-        else if(s.charAt(0) == s.charAt(s.length()-1)) {
-            s = s.substring(1, s.length()-1);
-            return isPalindrome(s);
-        }else {
+        else if(s.charAt(0) != s.charAt(s.length()-1)) {
             return false;
         }
+        s = s.substring(1, s.length()-1);
+        return isPalindrome(s);
     }
     
     public static void main(String[] args) {
